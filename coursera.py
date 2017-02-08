74af490e5d7c80802cc783677f81ec7cb294fa81
manchos <vladm.ru@gmail.com>
2017-02-08 19:05:30 +0300

    parsing attempt
    mend

--- coursera.py
+++ coursera.py
@@ -4,6 +4,10 @@ from io import StringIO, BytesIO
 from bs4 import BeautifulSoup
 import requests
 import random
+import datetime
+from collections import namedtuple
+
+course_info_class = namedtuple('CourseInfo', ['name', 'language', 'date_start',])
 
 
 def get_courses_list():
@@ -12,6 +16,7 @@ def get_courses_list():
         courses_list_xml = etree.parse(f, parser)
     courses_list = [b.text for b in courses_list_xml.iter() if b.text.strip()]
     courses_list = random.sample(courses_list, 20)
+
     return courses_list
 
 
@@ -31,13 +36,19 @@ if __name__ == '__main__':
     # print(get_courses_list())
 
     url ='https://www.coursera.org/learn/abdomen-anatomy'
-    headers = {'User-agent': 'Mozilla/5.0'}
+    headers = {'User-agent': 'Mozilla/5.0', 'Accept-Encoding': 'gzip'}
     response = requests.get(url, headers=headers)
+    print(response.encoding)
     print(response.status_code)  # Код ответа
     print(response.headers) # Заголовки ответа
-    # print(response.content) # Тело ответа
+
+
     soup = BeautifulSoup(response.text, 'lxml')
-    title = soup.select('.title .display-3-text')
+    title = soup.select('div.title.display-3-text')[0]
     title1 = soup.find('div', {'class': 'title display-3-text'})
-    # title = soup.find_all("div", class_="title display-3-text")
-    print(title1.text)
+    start = soup.find_all(attrs={"data-reactid": "82"})[0]
+    weeks_number = soup.find_all(attrs={"data-reactid": "211"})[0]
+    #data-reactid="211"
+    print(start.text)
+    print(weeks_number.text)
+    # datetime.strptime(start.text[],"%b %d")
