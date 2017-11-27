9906aa8b659c559bb44b6113d4e0152666cd8882
manchos <vladm.ru@gmail.com>
2017-11-27 18:09:27 +0300

    add function get_courses_list()

--- coursera.py
+++ coursera.py
@@ -9,15 +9,27 @@ from collections import namedtuple
 
 course_info_class = namedtuple('CourseInfo', ['name', 'language', 'date_start',])
 
+courses_list = ''
 
-def get_courses_list():
-    parser = etree.XMLParser(recover=True)
-    with urllib.request.urlopen('https://www.coursera.org/sitemap~www~courses.xml') as f:
-        courses_list_xml = etree.parse(f, parser)
-    courses_list = [b.text for b in courses_list_xml.iter() if b.text.strip()]
-    courses_list = random.sample(courses_list, 20)
 
-    return courses_list
+# parser = etree.XMLParser(recover=True)
+# courses_list_xml = etree.parse(f, parser)
+# courses_list = [b.text for b in courses_list_xml.iter() if b.text.strip()]
+# courses_list = random.sample(courses_list, 20)
+
+
+def get_random_courses_list(courses_xml, tag='loc'):
+    soup = BeautifulSoup(courses_xml, 'lxml')
+    return [url.string for url in soup.find_all(tag, string=True)]
+
+
+def get_courses_xml(url = 'https://www.coursera.org/sitemap~www~courses.xml', courses_limit = 20):
+    # url ='https://www.coursera.org/learn/abdomen-anatomy'
+    headers = {'User-agent': 'Mozilla/5.0', 'Accept-Encoding': 'gzip'}
+    try:
+        return requests.get(url, headers=headers).text
+    except ConnectionError:
+        return None
 
 
 def get_course_info(course_slug):
@@ -35,20 +47,29 @@ if __name__ == '__main__':
 if __name__ == '__main__':
     # print(get_courses_list())
 
-    url ='https://www.coursera.org/learn/abdomen-anatomy'
-    headers = {'User-agent': 'Mozilla/5.0', 'Accept-Encoding': 'gzip'}
-    response = requests.get(url, headers=headers)
-    print(response.encoding)
-    print(response.status_code)  # Код ответа
-    print(response.headers) # Заголовки ответа
+
+    courses_xml = get_courses_xml()
+    # print(courses_xml)
+    # soup = BeautifulSoup(response.text, 'lxml')
+    print(get_random_courses_list(courses_xml))
+
+    # print(response.encoding)
+    # print(response.status_code)  # Код ответа
+    # print(response.headers) # Заголовки ответа
 
 
-    soup = BeautifulSoup(response.text, 'lxml')
-    title = soup.select('div.title.display-3-text')[0]
-    title1 = soup.find('div', {'class': 'title display-3-text'})
-    start = soup.find_all(attrs={"data-reactid": "82"})[0]
-    weeks_number = soup.find_all(attrs={"data-reactid": "211"})[0]
-    #data-reactid="211"
-    print(start.text)
-    print(weeks_number.text)
-    # datetime.strptime(start.text[],"%b %d")
+
+    # aa = [i for i in soup.root]
+    # get_random_courses_list
+    # courses_url_list = soup.find_all('loc', string=True)
+
+    # courses_url_list = [url.string for url in soup.find_all('loc', string=True)]
+
+    # title = soup.select('div.title.display-3-text')[0]
+    # title1 = soup.find('div', {'class': 'title display-3-text'})
+    # start = soup.find_all(attrs={"data-reactid": "82"})[0]
+    # weeks_number = soup.find_all(attrs={"data-reactid": "211"})[0]
+    # #data-reactid="211"
+    # print(start.text)
+    # print(weeks_number.text)
+    # # datetime.strptime(start.text[],"%b %d")
