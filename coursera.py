b4e34ad4ade406214265c483e6cfa9d4d8056bd6
manchos <vladm.ru@gmail.com>
2017-11-29 18:04:29 +0300

    add argparse request_cache and get courses numedtuple list

--- coursera.py
+++ coursera.py
@@ -8,11 +8,13 @@ import random
 from datetime import datetime
 from collections import namedtuple
 import re
+import requests_cache
 import sys
+import os
+import argparse
 
-course_info_class = namedtuple('CourseInfo', ['name', 'lang', 'date_start', 'weeks_number', "rating"])
-
-
+course_info_class = namedtuple('CourseInfo',
+                               ['name', 'url', 'lang', 'start_date', 'duration_in_weeks', 'rating'])
 
 # parser = etree.XMLParser(recover=True)
 # courses_list_xml = etree.parse(f, parser)
@@ -20,16 +22,21 @@ course_info_class = namedtuple('CourseInfo', ['name', 'lang', 'date_start', 'wee
 # courses_list = random.sample(courses_list, 20)
 
 
-def get_random_courses_list(courses_xml, tag='loc'):
+def get_random_courses_url_list(courses_xml, tag='loc'):
     soup = BeautifulSoup(courses_xml, 'lxml')
     return random.sample([url.string for url in soup.find_all(tag, string=True)], 20)
 
 
-def get_courses_xml(url = 'https://www.coursera.org/sitemap~www~courses.xml', courses_limit = 20):
+def get_courses_info_list(courses_url_list):
+    return [get_course_info(course_url) for course_url in courses_url_list]
+
+
+def get_courses_xml(url='https://www.coursera.org/sitemap~www~courses.xml'):
     headers = {'User-agent': 'Mozilla/5.0', 'Accept-Encoding': 'gzip'}
     try:
         return requests.get(url, headers=headers).text
-    except ConnectionError:
+    except (ConnectionError, requests.exceptions.ConnectionError) as exc:
+        print(exc)
         return None
 
 
@@ -55,82 +62,93 @@ def get_open_repo_issues_list(repo_owner, repo_name):
     return [repo_issue for repo_issue in repo_issues_dict if 'pull_request' not in repo_issue]
 
 
+def get_course_start_datetime(soup):
+    start_date_text = \
+        soup.find('div', {'class': 'startdate rc-StartDateString caption-text'}).span.get_text()
+    try:
+        start_date_action_text = (start_date_text.split()[0])
+        start_date = (start_date_text.split()[:-3:-1])
+        start_datetime = datetime.strptime(' '.join(start_date), "%d %b")
+
+        # determine the year
+        if start_date_action_text == 'Начинается' and (start_datetime.month < datetime.now().month):
+            start_datetime = start_datetime.replace(year=datetime.now().year + 1)
+        else:
+            start_datetime = start_datetime.replace(year=datetime.now().year)
+        return start_datetime
+    except ValueError:
+        return start_date_text
+
+
+def get_course_rating_numeric_value(soup):
+    try:
+        rating_text = soup.find("div", "ratings-text bt3-hidden-xs").contents[1]
+        return re.findall(r'\d{1}.?\d?', rating_text)[0]
+    except AttributeError:
+        return None
+
+
 def get_course_info(course_url='https://www.coursera.org/learn/gis-capstone'):
-    course_info_dict = {}
-    # course_page = requests.get(course_url, headers=headers)
-    # название, язык, ближайшую дату начала, количество недель и среднюю оценку.
     course_response = get_course(course_url)
     if course_response:
-        soup = BeautifulSoup(course_response.text, 'lxml')
-        course_info_dict['name'] = soup.find('h1', {'class': 'title display-3-text'}).get_text()
-        course_info_dict['lang'] = soup.find('div', attrs={'class': 'rc-Language'}).contents[1]
-        start_date = \
-            soup.find('div', {'class': 'startdate rc-StartDateString caption-text'}).span.get_text()
-        # course_info_dict['weeks_number'] = soup.find('td', {'class': 'td-data'}).string
-        # datetime.strptime(''.join(start_date.split()[:-3:-1]), "%d %b")
-
-        aa = (start_date.split()[:-3:-1])
-        aa.append(str(datetime.now().year))
-        course_info_dict['start_date'] = aa
-        course_info_dict['start_date'] = datetime.strptime(' '.join(course_info_dict['start_date']), "%d %b %Y")
-
-        #     soup.find("i", {'class': 'cif-clock'}).find_parent('tr').find('td', string=True).get_text()
-
-        course_info_dict['duration'] = '{number} weeks'.format(number=len(soup.find_all('div', {'class': 'week'})))
-
-        # half_scores = soup.find('div', {"class": "rc-CourseRatingIcons"}).\
-        #     find_all('i','c-course-rating-icon cif-star-half-empty')
-        # scores = soup.find('div', {"class": "rc-CourseRatingIcons"}).find_all('i', 'c-course-rating-icon cif-star')
-        # course_info_dict['rating'] = len(scores) + len(half_scores) / 2
-        course_info_dict['rating'] = soup.find("div", "ratings-text bt3-hidden-xs").contents[1]
-        course_info_dict['rating'] = re.findall(r'\d{1}.?\d?', course_info_dict['rating'])[0]
-        print(course_info_dict)
-
-        # return repository_info_class(repo['owner']['login'], repo['name'], repo["html_url"],
-        #                           get_open_repo_issues_list(repo['owner']['login'], repo['name']))
-        # print(course_html.status_code)  #Код ответа
-        # print(course_html.headers) #Заголовки ответа
-
-        # return {key: utf8_encode(value, course_response.encoding) for key, value in course_info_dict.items()}
-    else:
-        return "Проверьте состояние подключения"
-    # rating = soup.find_all('i', 'c-course-rating-icon cif-star')
-
-
-
-    #data-reactid="211"
-
-    # print(utf8_encode(title1.text, response.encoding))
-    # print(utf8_encode(weeks_number, response.encoding))
-    # print(utf8_encode(start, response.encoding))
-    # print(rating)
-    # print(utf8_encode(weeks_number, response.encoding))
-
-    # print(weeks_number.text)
-    # datetime.strptime(start.text[],"%b %d")
+        soup = BeautifulSoup(utf8_encode(course_response.text, course_response.encoding), 'lxml')
+        course_info = course_info_class(
+            name=soup.find('h1', {'class': 'title display-3-text'}).get_text(),
+            url=course_url,
+            lang=soup.find('div', attrs={'class': 'rc-Language'}).contents[1],
+            start_date=get_course_start_datetime(soup),
+            duration_in_weeks='{number} weeks'.format(number=len(soup.find_all('div', {'class': 'week'}))),
+            rating=get_course_rating_numeric_value(soup)
+        )
+        return course_info
 
 
 def output_courses_info_to_xlsx(filepath):
     pass
 
 
-if __name__ == '__main__':
-    pass
+def set_cli_argument_parse():
+    parser = argparse.ArgumentParser(description="Displays 20 the most popular repositories")
+    parser.add_argument("-cachetime", "--cache_time", default=1200, type=int,
+                        dest="cache_time", help="Set cache time interval")
+    parser.add_argument('-clearcache', '--clear_cache', action='store_true', help='Clear cache file')
+    return parser.parse_args()
 
-
 if __name__ == '__main__':
+    cli_argument_parser = set_cli_argument_parse()
+
+    if not os.path.exists('_cache'):
+        os.mkdir('_cache')
+    requests_cache.install_cache('_cache/page_cache', backend='sqlite',
+                                 expire_after=cli_argument_parser.cache_time)
+
+    if cli_argument_parser.clear_cache:
+        requests_cache.clear()
+
+    courses_xml = get_courses_xml()
+    courses_list = get_random_courses_url_list(courses_xml)
+    courses_info_list = get_courses_info_list(courses_list)
     # print(get_courses_list())
 
-    # print(courses_xml)
+    print(courses_info_list )
     # soup = BeautifulSoup(response.text, 'lxml')
 
     # courses_xml = get_courses_xml()
     # print(get_random_courses_list(courses_xml))
 
-    get_course_info()
+    # get_course_info()
+
+
     # print(response.encoding)
     # print(response.status_code)  # Код ответа
     # print(response.headers) # Заголовки ответа
+
+    # course_url = 'https://www.coursera.org/learn/gis-capstone'
+    # course_response = get_course('https://www.coursera.org/learn/assembling-genomes')
+    # soup = BeautifulSoup(utf8_encode(course_response.text, course_response.encoding), 'lxml')
+    # aa = get_course('https://www.coursera.org/learn/assembling-genomes')
+    # print(get_course_info('https://www.coursera.org/learn/gis-capstone'))
+
 
 
 
